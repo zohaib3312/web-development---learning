f52fea414c57f60be2343617303290450b7c6779
Zohaib Akhtar <103579369+zohaib3312@users.noreply.github.com>
2026-03-04 01:54:48 +0500

    javascript pratice

--- script.js
+++ script.js
@@ -53,11 +53,127 @@
 
 // Exercise 2 — Do the same with var and see that it works outside. Notice the difference.
 
-{
-    var blockvariable = "I am inside a block";
-}
+// {
+//     var blockvariable = "I am inside a block";
+// }
 
 
-console.log(blockvariable); // This will work because var is function-scoped, not block-scoped.
+// console.log(blockvariable); // This will work because var is function-scoped, not block-scoped.
+
+
+
+
+
+
+// Create variables for your name, age, city, and whether you are a student. Use let or const correctly for each one.
+
+
+// const name = "zohaib";
+// let age = 25;
+// let city = "karachi";
+
+// create two number variables and print their sum, difference, and multiplication. Use console.log() to print. 
+
+
+// let a = 50;
+// let b = 20;
+
+// console.log("sum" , a+b);//sum
+// console.log("multiplication" , a*b); // multiplication
+// console.log("difference" ,  a-b); // difference
+
+
+
+// // Create a variable with no value assigned. Print it — what do you see? Now assign a value and print again.
+// let a;
+// console.log(a); //undefined 
+// a = 25;
+// console.log(a); // get 25 value
+
+
+
+//  exercise 4 — store your first and last name in two separate variables, combine them into one full name and print it using template literals 
+
+// let Firstname = "zohaib";
+// let Lastname = "Akhtar"
+
+// console.log(`${Firstname} ${Lastname}`) // zohiab Akhtar
+
+
+//  exercise 5 — store any sentence in a variable and print its length.
+
+// let sentence = "he is driving a car";
+// console.log(sentence.length); // 19 length
+
+
+
+
+// create an object for a phone with these fields: brand, model, price, isAvailable. Print each field using dot notation. 
+
+// let phone = {
+// brand: "samsung",
+// model: "3rd generation",
+// price: 25000,
+// isAvailable: true
+// };
+
+// console.log(phone.brand);
+// console.log(phone.model);
+// console.log(phone.price);
+// console.log(phone.isAvailable);
+
+// exercise 7 — take your phone object, change the price and add a new field called color. Then print the full object using console.log(phone)
+
+// let phone = {
+// brand: "samsung",
+// model: "3rd generation",
+// price: 25000,
+// isAvailable: true
+// };
+
+//  phone.price = 30000;
+// phone.color= "black"
+// console.log(phone)
+
+
+//  exercise 8 — create an object for yourself with at least 5 fields:
+
+// let userInfo = {
+// name : "zohaib",
+// age : 25,
+// Email:"za809475@gmail.com",
+// phoneNo: "03156113665",
+// is18yearold: true
+// };
+
+// Now try exercise 9 — why does this cause an error? Fix it:
+
+// let country = "Pakistan";
+// country = "India";
+// console.log(country)
+
+
+
+//  exercise 10 — the last one!
+// Write one example of null and one example of undefined with a comment explaining why you used each one
+
+
+//null is intiontilly leave the varialbe empthy 
+// undefined mean varialbe is declared but not assign any value or forget to assign the value
+
+
+
+// let car; //undefined 
+// let name = null // intionlly leave empthy 
+
+
+
+
+
+ 
+
+
+
+
 
 
