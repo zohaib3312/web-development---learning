5aeef6d9e78c20d44bddd0e061ccd52e9e595121
Zohaib Akhtar <103579369+zohaib3312@users.noreply.github.com>
2026-03-17 01:49:38 +0500

    pratice

--- script.js
+++ script.js
@@ -933,3 +933,18 @@ showProduct()
 totalPrice()
 
  
+let result = 1
+
+for (let i = 1; i <= 5; i++) {
+  result *= i;
+  
+}
+console.log(result);
+
+
+
+let factoral = [1,2,3,4,5];
+
+let result1 = factoral.reduce((num, sum)=>num * sum , 1);
+
+console.log(result1);
